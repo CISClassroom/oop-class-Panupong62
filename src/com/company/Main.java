554157860bb6f1df18fe0971269b48d446ba2cf9
Panupong62 <59049140+Panupong62@users.noreply.github.com>
2019-12-20 16:40:58 +0700

    เเก้ไข

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,38 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	   // write your code here
+       Student mark = new Student();
+       Student tontan = new Student();
+       Student dream = new Student();
+
+
+       //input data to Object
+        mark.name ="x mark";
+        mark.id =  "001";
+        mark.major = "CIS";
+
+        tontan.name ="ton tan";
+        tontan.id =  "002";
+        tontan.major = "CIS";
+
+        dream.name = "dream";
+        dream.id = "003";
+        dream.major = "CS";
+
+
+        //get data from Object
+        System.out.println("1." + mark.name +" " + mark.major);
+        System.out.println("2." +tontan.name + " " + tontan.major);
+        System.out.println("3." +dream.name + "" + dream.major );
+
+        //odd student to array
+        Student[] oopStudents = {mark,tontan,dream};
+        System.out.println("1." + oopStudents[0].name);
+        System.out.println("2." + oopStudents[1].name);
+        System.out.println("2." + oopStudents[2].name);
+
+
     }
+    
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Student {
+    //ประกาศตัวแปปล Class
+    public String name;
+    public String id;
+    public String major;
+}
